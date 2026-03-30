afef433ffa020400a0502848cb707f4531a4d525
dladyddn133-creator <dladyddn133@gmail.com>
2026-03-30 09:53:05 +0900

    timer update

--- 1102.TIMER_DRIVER_LAB/timer.c
+++ 1102.TIMER_DRIVER_LAB/timer.c
@@ -1,8 +1,10 @@
 #include "device_driver.h"
 #define TIM2_TICK         	(20) 				// usec
 #define TIM2_FREQ 	  		(1000000/TIM2_TICK)	// Hz
-#define TIME2_PLS_MS  	(1000/TIM2_TICK)
+#define TIME2_PLS_MS  		(1000/TIM2_TICK)
 #define TIM2_MAX	  		(0xffffu)
+
+#define TIME4_PLS_OF_1ms 	(1000/TIM2_TICK)
 
 void TIM2_Stopwatch_Start(void)
 {
@@ -64,16 +66,30 @@ void TIM4_Repeat(int time)
 	Macro_Set_Bit(RCC->APB1ENR, 2);
 
 	// TIM4 CR1: ARPE=0, down counter, repeat mode
+	TIM4->CR1 = (1<<4)|(0<<3);
 	// PSC(50KHz),  ARR(reload시 값) 설정
+	TIM4->PSC = (unsigned int)(TIMXCLK/(double)TIM4_FREQ + 0.5)-1;
+	TIM4->ARR = TIME4_PLS_OF_1ms * time - 1;
 	// UG 이벤트 발생
+	Macro_Set_Bit(TIM4->EGR,0);
 	// Update Interrupt Pending Clear
+	Macro_Clear_Bit(TIM4->SR, 0);
 	// TIM4 start
-
+	Macro_Set_Bit(TIM4->CR1, 0);
 }
 
 int TIM4_Check_Timeout(void)
 {
 	// 타이머가 timeout 이면 1 리턴, 아니면 0 리턴
+	if(Macro_Check_Bit_Set(TIM4->SR, 0))
+	{
+		Macro_Clear_Bit(TIM4->SR, 0);
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 
 }
 
