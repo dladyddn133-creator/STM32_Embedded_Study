12e05beb8fcd67988c97b609c88bbc27771fb06a
dladyddn133-creator <dladyddn133@gmail.com>
2026-03-30 10:17:46 +0900

    timer update

--- 1102.TIMER_DRIVER_LAB/timer.c
+++ 1102.TIMER_DRIVER_LAB/timer.c
@@ -1,10 +1,12 @@
 #include "device_driver.h"
 #define TIM2_TICK         	(20) 				// usec
 #define TIM2_FREQ 	  		(1000000/TIM2_TICK)	// Hz
-#define TIME2_PLS_MS  		(1000/TIM2_TICK)
+#define TIM2_PLS_MS  		(1000/TIM2_TICK)
 #define TIM2_MAX	  		(0xffffu)
 
 #define TIME4_PLS_OF_1ms 	(1000/TIM2_TICK)
+#define TIM4_FREQ 	  		(1000000/TIM2_TICK)	// Hz
+
 
 void TIM2_Stopwatch_Start(void)
 {
@@ -53,7 +55,7 @@ void TIM2_Delay(int time)
 	// UIF(Update Interrupt Pending) Clear
 	Macro_Clear_Bit(TIM2->SR,0);
 	// TIM2 start
-	Macro_Set_Bit(TIM2->CR,0);
+	Macro_Set_Bit(TIM2->CR1,0);
 	// Wait timeout
 	while(Macro_Check_Bit_Clear(TIM2->SR,0));
 
